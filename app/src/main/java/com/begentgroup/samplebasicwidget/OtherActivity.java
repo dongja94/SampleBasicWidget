c74c195e61020e32b6789131e5dea7f82bf4685d
dongja94 <dongja94@gmail.com>
2015-10-05 10:10:29 +0900

    TextChangeListener

--- app/src/main/java/com/begentgroup/samplebasicwidget/OtherActivity.java
+++ app/src/main/java/com/begentgroup/samplebasicwidget/OtherActivity.java
@@ -1,13 +1,55 @@
 package com.begentgroup.samplebasicwidget;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class OtherActivity extends AppCompatActivity {
+
+    EditText passwordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+        passwordView = (EditText)findViewById(R.id.editText2);
+        passwordView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String text = s.toString();
+                if (text.length() < 6) {
+                    passwordView.setTextColor(Color.RED);
+                } else {
+                    passwordView.setTextColor(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        passwordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    // ....
+                    Toast.makeText(OtherActivity.this, "click send key", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
     }
 }
